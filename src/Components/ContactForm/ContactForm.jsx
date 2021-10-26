501fbb66b6ea531668418cea48dddcf262cdb8a0
Misha Krasnonos <muzishutka7@gmail.com>
2021-10-27 01:46:02 +0300

    Revert "up"
    This reverts commit 1cec53a2b21b7fde8beb2bab71f5ea9893f4fc41.

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -48,7 +48,7 @@ const ContactForm = () => {
             onChange={handleChange}
             value={name}
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+[A-Za-zА-Яа-яЁёІіЇїЄє]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
           />
